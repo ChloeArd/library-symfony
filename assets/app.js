193fb81b7ba5b4ee1534752594bc34e9f95ef786
ChloeArd <chloe.ardoise@gmail.com>
2022-03-02 09:19:41 +0100

    Library with symfony

--- assets/app.js
+++ assets/app.js
@@ -15,3 +15,28 @@ import './bootstrap';
 
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
+
+let span = document.querySelectorAll("span");
+
+if (span) {
+    // each letter changes color and font style when hovering the mouse over a label
+    let color = ['blue', 'red', 'yellow', 'orange', 'green', 'black', 'brown', 'gray', 'brown', 'blueviolet', 'coral', 'pink'];
+    let font = ['bold', 'normal'];
+
+    document.getElementById("titleLibrary").addEventListener("mouseover",letterColorAndFont);
+
+    function letterColorAndFont () {
+        let time = 500;
+        for (let x = 0; x < span.length; x++) {
+            setTimeout(function () {
+                let randomColor = color[Math.floor(Math.random() * color.length)];
+                let randomFont = font[Math.floor(Math.random() * font.length)];
+                span[x].style.color = randomColor;
+                span[x].style.fontWeight = randomFont;
+                span[x].style.fontStyle = "italic";
+                span[x].style.fontSize = "50px";
+            }, time);
+            time = time + 500;
+        }
+    }
+}
