e503ff94a1538bb620a213171bda332362e0d727
ChloeArd <chloe.ardoise@gmail.com>
2022-03-03 11:45:28 +0100

    Library with symfony

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -86,11 +86,15 @@ class BookController extends AbstractController {
         $form = $this->createForm(BookType::class, $book);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-            $this->addFlash("success", "Le livre a été modifié avec succès ! !");
-            $id = $book->getId();
-            return $this->redirect("/book/$id");
+        $submittedToken = $request->request->get("csrf_token");
+
+        if ($this->isCsrfTokenValid("book-add", $submittedToken)) {
+            if ($form->isSubmitted() && $form->isValid()) {
+                $entityManager->flush();
+                $this->addFlash("success", "Le livre a été modifié avec succès ! !");
+                $id = $book->getId();
+                return $this->redirect("/book/$id");
+            }
         }
 
         return $this->render('book/update.html.twig', ['form' => $form->createView()]);
