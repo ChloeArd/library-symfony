828381517e90b912600edaab705af1e30ddc340b
ChloeArd <chloe.ardoise@gmail.com>
2022-03-04 11:49:08 +0100

    Library with symfony

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\KernelEvents;
 
 class LocaleSubscriber implements EventSubscriberInterface
 {
-    // Langue par défaut
+    // Langue by default
     private $defaultLocale;
 
     public function __construct($defaultLocale = 'en')
